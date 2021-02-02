2722e455fb095a3044602bc8553df78809493e3f
hareesh-r <hareeshprogrammer@gmail.com>
2021-02-02 18:28:50 +0530

    change product.js

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Home.css";
+import Product from './Product.js';
 
 function Home() {
     return (
         <div className="home">
             <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" className="home__image" alt="Amazon Prime Banner Image Here"/>
+            <Product id="12345" title="Asus TUF A15" image="" price={62.999} rating={5} />
         </div>
     )
 }
--- src/Product.js
+++ src/Product.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./Product.css";
-function Product() {
+function Product({ id, title, image, price, rating }) {
     return (
-        <div>
-            
+        <div className="products">
+            <p>{title}</p>
+            <p className="product__price">
+                <span>₹</span>
+                <strong>{price}</strong>
+            </p>
+            <div className="product__rating">
+                {
+                    Array(rating).fill().map((i) => (<p>⭐</p>))
+                }
+            </div>
+            <img src="" alt={title+" image"} />
+            <button>Add to Cart</button>
         </div>
     )
 }
