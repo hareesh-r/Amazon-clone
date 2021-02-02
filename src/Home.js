56006b36a2fa1b53c89b93c2295b01d0fcf6d983
hareesh-r <hareeshprogrammer@gmail.com>
2021-02-02 18:47:29 +0530

    minor change product component

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,9 @@ import Product from './Product.js';
 function Home() {
     return (
         <div className="home">
-            <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" className="home__image" alt="Amazon Prime Banner Image Here"/>
-            <Product id="12345" title="Asus TUF A15" image="" price={62.999} rating={5} />
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" className="home__image" alt="Amazon Prime Banner Image Here" />
+
+            <Product id="12345" title="ASUS TUF Gaming A15 Laptop 15.6 FHD 144Hz Ryzen 5 4600H, GTX 1650 4GB Graphics (8GB RAM/1TB HDD + 256GB NVMe SSD/Windows 10/Bonfire Black/2.30 Kg), FA506IH-AL057T" image="https://www.reliancedigital.in/medias/Asus-BQ169T-Laptops-491894127-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3w1OTc3Njl8aW1hZ2UvanBlZ3xpbWFnZXMvaGRmL2hhMy85MzMwNDY4NDg3MTk4LmpwZ3xmZDZjYmMyNTFhYTgyNGYwM2RlYjc1ZGI0YTU5NjIxYmY4ODM2MzY0OGM3NzNkZDJkZGY0ZGRlMmEyZWYzN2Uy" price={62.999} rating={5} />
         </div>
     )
 }
--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import "./Product.css";
+
 function Product({ id, title, image, price, rating }) {
     return (
-        <div className="products">
-            <p>{title}</p>
-            <p className="product__price">
-                <span>₹</span>
-                <strong>{price}</strong>
-            </p>
-            <div className="product__rating">
-                {
-                    Array(rating).fill().map((i) => (<p>⭐</p>))
-                }
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <span>₹</span>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {
+                        Array(rating).fill().map((i) => (<p>⭐</p>))
+                    }
+                </div>
             </div>
-            <img src="" alt={title+" image"} />
+            <img src={image} alt={title + " image"} />
             <button>Add to Cart</button>
-        </div>
+        </div >
     )
 }
 
