eeb3a0ad5cd132e14248037116182189ea8f8a67
hareesh-r <hareeshprogrammer@gmail.com>
2021-02-02 20:50:01 +0530

    add to cart done

--- src/Header.js
+++ src/Header.js
@@ -7,6 +7,8 @@ import {useStateValue} from "./StateProvider";
 
 function Header() {
     const [{ basket}] = useStateValue();
+
+    console.log(basket);
     return (
         <nav className="header">
             <Link to="/">
--- src/reducer.js
+++ src/reducer.js
@@ -7,7 +7,10 @@ const reducer = (state,action) => {
     console.log(action)
     switch(action.type){
         case "ADD_TO_BASKET":
-            return {state};
+            return {
+                ...state,
+                basket: [...state.basket,action.item],
+            };
             break;
         case "REMOVE_FROM_BASKET":
             return {state};
