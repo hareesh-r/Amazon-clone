a224e79760a326a2ac480df2ba89dc5065ec1c58
hareesh-r <hareeshprogrammer@gmail.com>
2021-02-02 19:27:53 +0530

    minor change react context api

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -3,6 +3,7 @@ import React, { createContext , useContext, useReducer} from "react";
 export const StateContext = createContext();
 
 export const StateProvider = ({reducer , initialState , children}) => (
-    <StateContext.Provider value={useReducer(reducer,initialState)}>{childern}>
+    <StateContext.Provider value={useReducer(reducer,initialState)}>
+        {childern}
     </StateContext.Provider>
 )
