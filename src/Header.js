14ac72be167d851990917788d15c9205335023e3
hareesh <hareeshraji708@gmail.com>
2021-02-02 17:34:15 +0530

    Amazon Clone ReactJS

---
+++ src/Header.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import "./Header.css";
+function Header() {
+    return (
+        <nav className="header">
+            <Link to="/">
+                <img className="header__logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon Logo Here.." />
+            </Link>
+            <div className="header__search">
+                <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+            <div className="header__nav">
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Hello Hareesh</span>
+                        <span className="header__optionLineTwo">Sign in</span>
+                    </div>
+                </Link>
+
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Orders</span>
+                    </div>
+                </Link>
+
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
+                </Link>
+
+                <Link to="/checkout" className="header__link">
+                    <div className="header__optionBasket">
+                        <ShoppingCartIcon />
+                        <span className="header__optionLineTwo header__basketCount">0</span>
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="home">
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" className="home__image" alt="Amazon Prime Banner Image Here"/>
+        </div>
+    )
+}
+
+export default Home
